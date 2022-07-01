da48d1add2152e019d9e70b1a11cd1279ad87a83
andryjurca <andryjurca@gmail.com>
2022-07-01 07:37:38 +0200

    structured code

--- mapa.js
+++ mapa.js
@@ -3,6 +3,7 @@
 var geojsonphotos = {"type": "FeatureCollection","features": [{"type": "Feature","properties": {"filename": "img/crimsonu.jpg"},"geometry": {"type": "Point","coordinates": [26.109008789062496,44.43377984606822]}},{"type": "Feature","properties": {"filename": "img/biserica.jpg"},"geometry": {"type": "Point","coordinates": [26.102142333984375,44.422011314236634]}},{"type": "Feature","properties": {"filename": "img/arcul.jpg"},"geometry": {"type": "Point","coordinates": [26.09574794769287,44.43218632942285]}},{"type": "Feature","properties": {"filename": "img/dambovita.jpg"},"geometry": {"type": "Point","coordinates": [26.107420921325684,44.430531477686436]}},{"type": "Feature","properties": {"filename": "img/palatul.jpg"},"geometry": {"type": "Point","coordinates": [26.104202270507812,44.426363495332495]}},{"type": "Feature","properties": {"filename": "img/arcul.jpg"},"geometry": {"type": "Point","coordinates": [26.100382804870605,44.42804911222499]}},{"type": "Feature","properties": {"filename": "img/biserica.jpg"},"geometry": {"type": "Point","coordinates": [26.111712455749508,44.42752810855755]}},{"type": "Feature","properties": {"filename": "img/dambovita.jpg"},"geometry": {"type": "Point","coordinates": [26.109437942504883,44.42427939270172]}},{"type": "Feature","properties": {"filename": "img/palatul.jpg"},"geometry": {"type": "Point","coordinates": [26.095576286315918,44.42820234771396]}}]}
 
 // 2. functions
+
 var clicked_times = 0
 
 function changeMode(){
@@ -17,16 +18,10 @@ function changeMode(){
         document.getElementById('map').style.width = '50%';
         document.getElementById('picture').style.display = 'flex';
         map.invalidateSize();
-        map.setZoom(map.getZoom()-1)
-        // functie css cu care sa scalezi o imagine la dimensiunea divului
-        
-        
-    }
-    
+        map.setZoom(map.getZoom()-1)             
+    }   
 }
     
-
-
 // 3. create map
 
 var latlng = L.latLng(44.429, 26.105);
@@ -44,21 +39,7 @@ L.tileLayer('https://tiles01.rent-a-planet.com/arhet2-carto/{z}/{x}/{y}.png?{foo
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// 4. display meter
-
-L.control.scale({metric: true, imperial: false }).addTo(map);
-
-// 5. create test marker
-
-var marker = L.marker([51.5, -0.09], {
-        draggable: true,
-        title: "hover text",
-        opacity: 0.5,
-}).addTo(map);
-
-marker.bindPopup('<img src="img/crimsonu.jpg" width="100" height="100">');
-
-// 6. reading from geojson
+// 5. reading from geojson
 
 var myStyle = { // not working (uite-te mai mult la geojson styling)
         "color": "#ff7800",

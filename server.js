cccb13786c74ac8b936f272ce9ecd09d6d61f4a4
Andrei Jurcă <107988807+andryjurca@users.noreply.github.com>
2022-08-16 10:52:28 +0300

    cloudinary initialization

--- server.js
+++ server.js
@@ -134,7 +134,3 @@ cloudinary.config({
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET 
 });
-
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(`results ${result}`); });

24f46bb2ab7a8d014a41a4e4eccfba547339fc7e
Andrei Jurcă <107988807+andryjurca@users.noreply.github.com>
2022-10-01 20:36:07 +0200

    link to github repo

--- public/mapa.js
+++ public/mapa.js
@@ -80,7 +80,7 @@ const map = L.map('map', {
 
 L.tileLayer('https://tiles01.rent-a-planet.com/arhet2-carto/{z}/{x}/{y}.png?{foo}', {
         foo: 'bar', 
-        attribution: 'Tiles and data created by <a href="https://geofictician.net/about.html">Geofictician</a> and contributors | coded by Andrei Jurcă'
+        attribution: 'Tiles and data created by <a href="https://geofictician.net/about.html">Geofictician</a> and contributors | coded by <a href="https://github.com/andryjurca/pictures-map">Andrei Jurcă</a>'
 }).addTo(map);
 
 // marker icons and styles
